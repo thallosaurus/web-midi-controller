7d5e97b29e25ef34f59af3be7dbd61c44c247bf4
Philipp Gaßner <activator112233@gmail.com>
2026-01-24 06:17:30 +0100

    added connection status

--- web/src/websocket.ts
+++ web/src/websocket.ts
@@ -2,9 +2,13 @@ const wsUri = "ws://" + location.hostname + ":8080";
 const ws = new WebSocket(wsUri);
 ws.onopen = () => {
     console.log("connection")
+    document.querySelector<HTMLDivElement>("#connection_status")!.innerText = "connected";
 }
 ws.onmessage = (e) => {
     console.log(e);
+}
+ws.onclose = () => {
+    document.querySelector<HTMLDivElement>("#connection_status")!.innerText = "disconnected";
 }
 
 export default ws;
