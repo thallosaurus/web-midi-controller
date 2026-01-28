fa92c2847d7f84e59d58f48ae06c8be44db5e980
Philipp Gaßner <activator112233@gmail.com>
2026-01-28 23:49:07 +0100

    better socket handling

--- web/ts/main.ts
+++ web/ts/main.ts
@@ -2,10 +2,10 @@ import type { Overlay } from "../bindings/Overlay.ts";
 import { init_event_bus } from "./event_bus.ts";
 import "./style.css";
 //import { setup_ccbutton, setup_notebutton } from "./ui/button.ts";
-import { change_overlay, register_overlay, setup_tabs } from "./ui/overlay.ts";
+import { change_overlay, init_overlays, register_overlay, setup_tabs } from "./ui/overlay.ts";
 import { render_overlay } from "./ui/render.ts";
 //import { setup_slider } from "./ui/slider.ts";
-import { connect_local } from "./websocket.ts";
+import { connect, disconnect } from "./websocket.ts";
 import { close_dialog, init_dialogs, open_dialog } from './dialogs.ts'
 import { init_debug } from "./utils.ts";
 
@@ -13,7 +13,8 @@ const init = async () => {
   //init_debug();
   init_event_bus();
   init_dialogs();
-  connect_local();
+  await connect();
+  //await connect_local();
 
   /*const overlays_parent = document.querySelector<HTMLDivElement>(
     "main#overlays",
@@ -21,32 +22,13 @@ const init = async () => {
   if (import.meta.env.DEV) {
     console.log("dev");
   }
-  const overlays = await fetch(
-    import.meta.env.DEV ? "demo_overlay.json" : "overlays",
-  );
+  
   //console.log(await overlays.json());
 
-  const ol: Array<Overlay> = await overlays.json();
+  await init_overlays();
 
-  for (
-    //const overlay of document.querySelectorAll<HTMLDivElement>("div.overlay")!
-    const overlay of ol
-  ) {
-    // Test Render Flow here
-    const oo = render_overlay(overlay);
-    register_overlay(oo);
-    console.log(oo);
-    //debugger;
-  }
 
-  const overlay_selector = document.querySelector<HTMLDivElement>(
-    "#overlay_selector",
-  )!;
-  setup_tabs(ol, overlay_selector, (i) => {
-    console.log("setting tab ", i)
-    change_overlay(i);
-    close_dialog("overlay_menu")
-  });
+
   
   /*const slide_menu = document.querySelector<HTMLDivElement>(
     "#overlay_menu_activator",
@@ -76,8 +58,10 @@ self.addEventListener("DOMContentLoaded", () => {
 
 document.querySelector<HTMLDivElement>("#connection_status")!.addEventListener(
   "click",
-  (_ev) => {
-    connect_local();
+  async (_ev) => {
+    disconnect();
+    connect();
+    //connect_local();
   },
 );
 
