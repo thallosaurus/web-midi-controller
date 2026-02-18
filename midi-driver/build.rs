63833a776a4acb388617492a0eb53c0944720dec
Philipp Gaßner <activator112233@gmail.com>
2026-02-18 23:22:51 +0100

    enabled use of midi-driver as system library

--- midi-driver/build.rs
+++ midi-driver/build.rs
@@ -1,7 +1,20 @@
+const PKG_CONF: &'static str = "prefix=/usr
+libdir=${prefix}/lib
+includedir=${prefix}/include
+
+Name: midicore
+Description: MIDI control runtime
+Version: 0.1.11
+
+Libs: -L${libdir} -lmidicore
+Cflags: -I${includedir}";
+
 fn main() {
     let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
 
     cbindgen::generate(crate_dir)
         .unwrap()
         .write_to_file("native/libmidi_driver.h");
+
+    std::fs::write("native/libmidi_driver.pc", PKG_CONF).unwrap();
 }
