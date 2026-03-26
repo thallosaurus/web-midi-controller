94b73665e392a1052a6768432d47a0eb6dd3f3b5
Lenna Mothburn <activator112233@gmail.com>
2026-03-26 17:04:35 +0100

    nix builds the midi driver

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+#    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+#    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    packages.x86_64-linux.midi-driver = pkgs.rustPlatform.buildRustPackage {
+      
+          pname = "myapp";
+          version = "0.1.12";
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            pkgconf
+          ];
+          buildInputs = with pkgs; [
+            alsa-lib.dev
+          ];
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+        };
+      
+    packages.x86_64-linux.default = self.packages.x86_64-linux.midi-driver;
+  };
+}
