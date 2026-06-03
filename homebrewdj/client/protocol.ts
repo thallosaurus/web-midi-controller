1db1b41c24515563d6d77c4e3f00b3ea54b6fcf8
Lenna Mothburn <activator112233@gmail.com>
2026-06-03 23:06:07 +0200

    i run out of space

--- homebrewdj/client/protocol.ts
+++ homebrewdj/client/protocol.ts
@@ -1,4 +1,4 @@
-interface ConnectedPayload {
+export interface ConnectedPayload {
     type: "connection",
     id: string
 }
--- homebrewdj/client/websocket.ts
+++ homebrewdj/client/websocket.ts
@@ -1,28 +1,46 @@
-import { AllowedPayloads } from "./protocol.ts";
+import { AllowedPayloads, ConnectedPayload } from "./protocol.ts";
 
 type WebsocketMessageCallback<T> = (msg: T) => void;
 
+export function asyncWebsocketClient<T>(endpoint: URL, handler: WebsocketMessageCallback<T>) {
+    return new Promise((res, rej) => {
+        return new WebsocketClient<T>(endpoint, handler, res, rej);
+    })
+}
+
 export class WebsocketClient<T = AllowedPayloads> {
     private ws: WebSocket
-    constructor(endpoint: URL, handler: WebsocketMessageCallback<T> = console.log) {
+    private id: string | null = null
+
+    constructor(endpoint: URL, 
+        handler: WebsocketMessageCallback<T>,
+        open?: (id: string) => void,
+        close?: (reason?: string) => void
+    ) {
         const ws = new WebSocket(endpoint);
         //handler(ws);
 
         ws.addEventListener("open", (ev) => {
             console.log(ev);
+            const evt = (ev as CustomEvent<ConnectedPayload>).detail;
+            if (evt.type == "connection") {
+                this.id = evt.id
+                if (open) open(evt.id)
+            }
         })
 
         ws.addEventListener("close", (ev) => {
-            console.log(ev);
+            this.id = null;
+            if (close) close(ev.reason);
         })
 
         ws.addEventListener("message", (ev) => {
-            //console.log(JSON.parse(ev.data) as T);
             handler(JSON.parse(ev.data))
         })
 
         ws.addEventListener("error", (ev) => {
             console.log(ev);
+            if (close) close()
         })
         this.ws = ws;
     }
