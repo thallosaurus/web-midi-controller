bd653455201b0c99497450c32f4e7dd77f29604e
Lenna Mothburn <activator112233@gmail.com>
2026-06-05 19:45:08 +0200

    what am i even doing anymore

--- homebrewdj/default.nix
+++ homebrewdj/default.nix
@@ -5,9 +5,21 @@ let
     sha256 = "sha256:1ri5z1pb23kyrpy0j37s2kh95bk3r12jzscck4bigrsw9755iyai";
   };
 
-  oak = builtins.fetchurl {
-    url = "https://deno.land/x/oak/mod.ts";
-    sha256 = "sha256:164fqqkpskpf1l4j7s6i2ww92ajdprr9p3kvmz70wc9qc4d38hhz";
+  #oak = builtins.fetchurl {
+  #  url = "https://deno.land/x/oak/mod.ts";
+  #  sha256 = "sha256:164fqqkpskpf1l4j7s6i2ww92ajdprr9p3kvmz70wc9qc4d38hhz";
+  #};
+  oak = stdenv.mkDerivation {
+    name = "oak-deno";
+    src = pkgs.fetchFromGitHub {
+      owner = "oakserver";
+      repo = "oak";
+      rev = "v17.2.0";
+      hash = "sha256-2fDXedLE5z7ZRT5H6kWEH6EehgoQQN5jrfPEMcWDrOs=";
+    };
+    installPhase = ''
+      cp -r . $out
+    '';
   };
 
   importmap = pkgs.writeTextFile {
@@ -20,7 +32,7 @@ let
         "@launchpad": "${launchpad-driver}/main.ts",
         "@driver": "${midi-driver}/deno/index.ts",
         "@driver-deno": "${midi-driver}/deno/ffi.ts",
-        "oak": "${oak}"
+        "oak": "${oak}/mod.ts"
       }
     }
   '';
@@ -34,8 +46,10 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out
     ln -s ${importmap} $out/deno.json
+    deno cache --import-map ${importmap} --lock $out/deno.lock main.ts
     cp main.ts $out/main.ts
     cp server.ts $out/server.ts
+    cp -r client/ $out/client/
     cp ${midi-driver}/lib/libmidi_driver.so $out/libmidi_driver.so
   '';
     #//deno cache --import-map ${deno-config} --lock deno.lock main.ts
