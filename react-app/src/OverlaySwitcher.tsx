0a24010a88e1dec62143f16774e3ecf1f015a11b
Lenna Mothburn <activator112233@gmail.com>
2026-06-11 03:23:29 +0200

    fixed rotaries

--- react-app/src/OverlaySwitcher.tsx
+++ react-app/src/OverlaySwitcher.tsx
@@ -1,6 +1,6 @@
 import { Overlay } from "@hdj/definitions";
 import { useRef, useLayoutEffect } from "react";
-import { VOLUME_SLIDER_OVERLAY, VOLUME_SLIDER_OVERLAY_NEW, XYPAD_OVERLAY, MATRIX_OVERLAY, ABLETON_OVERLAY } from "./Overlays";
+import { VOLUME_SLIDER_OVERLAY, VOLUME_SLIDER_OVERLAY_NEW, XYPAD_OVERLAY, MATRIX_OVERLAY, ABLETON_OVERLAY, TRAKTOR_PERFORMANCE } from "./Overlays";
 
 export function OverlaySwitcher({ showModal, closeSwitcher, setOverlay }: { showModal: boolean, closeSwitcher: () => void, setOverlay: (o: Overlay) => void }) {
   const overlays = useRef([
@@ -8,7 +8,8 @@ export function OverlaySwitcher({ showModal, closeSwitcher, setOverlay }: { show
     VOLUME_SLIDER_OVERLAY_NEW,
     XYPAD_OVERLAY,
     MATRIX_OVERLAY,
-    ABLETON_OVERLAY
+    ABLETON_OVERLAY,
+    TRAKTOR_PERFORMANCE
   ])
 
   const dialogRef = useRef<HTMLDialogElement>(null);
