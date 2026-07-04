0755ac3b978be72870fbd6ea6ec1b96ae4e9074b
Lenna Mothburn <activator112233@gmail.com>
2026-07-04 20:40:12 +0200

    my head hurts

--- react-app/src/vite-env.d.ts
+++ react-app/src/vite-env.d.ts
@@ -1,8 +1,9 @@
+/// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_BACKEND: string,
   readonly VITE_VERSION: string,
 }
 
 interface ImportMeta {
-    readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv
 }
